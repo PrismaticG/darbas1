80230e02039b7ffe79a091e1c274f5f136b21da1
Aldas Dovidauskas <aldas.dov7@gmail.com>
2024-10-01 19:53:13 +0300

    Revert "Button ChangeColor funkcionalumas"
    This reverts commit 9f431992a505c6ae830047dabd27a2e64e340484.

--- app/src/main/java/com/example/darbas/MainActivity.java
+++ app/src/main/java/com/example/darbas/MainActivity.java
@@ -30,14 +30,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnChangeColor=findViewById(R.id.btnChangeColor);
-        btnChangeColor.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View view) {
-                                                 tvMain = findViewById(R.id.tvMain);
-                                                 tvMain.setTextColor(Color.GREEN);
-                                             }
-                                         });
+
         btnChangeText=findViewById(R.id.btnChangeText);
         btnChangeText.setOnClickListener(new View.OnClickListener() {
         @Override
