8f3dd7e48d54b70671b725322d9838edaef723a7
Aldas Dovidauskas <aldas.dov7@gmail.com>
2024-10-01 19:47:37 +0300

    Button2 funkcionalumas

--- app/src/main/java/com/example/darbas/MainActivity.java
+++ app/src/main/java/com/example/darbas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.darbas;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
     Button btnChangeText;
+    Button btnChangeColor;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnChangeColor=findViewById(R.id.btnChangeColor);
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View view) {
+                                                 tvMain = findViewById(R.id.tvMain);
+                                                 tvMain.setTextColor(Color.GREEN);
+                                             }
+                                         });
         btnChangeText=findViewById(R.id.btnChangeText);
         btnChangeText.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -34,5 +46,5 @@ public class MainActivity extends AppCompatActivity {
             tvMain.setText("Hello");
         }
     });
-    };
+    }
 }
